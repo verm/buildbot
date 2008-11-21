d91f856ffc81f4c76835b970630418885776275c
Dustin J. Mitchell <dustin@zmanda.com>
2008-11-21 12:05:12 -0500

    add a configuration that can evolve along with the hacking I'm doing here

---
+++ TMP/buildmaster_cfg.py
@@ -0,0 +1,25 @@
+# -*- python -*-
+# ex: set syntax=python:
+
+import re
+
+#srcmgr = addSourceManager(
+#    DirectorySourceManager(
+#        name="dir",
+#        directory="/tmp/BB",
+#        isImportant = re.compile(r".*\.tar\.gz$").match,
+#        interval=1,
+#    ))
+
+
+#sched = addScheduler(
+#    BuildChangesScheduler(
+#        name="buildit",
+#        sourceManager=srcmgr,
+#    ))
+
+#sl = addSlave(
+#    Slave(
+#        name="worker",
+#        password="hi",
+#    ))
