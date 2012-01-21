0fffaa529bbd2ba7fa0ac3e26b10a3c13b801c4b
Amar Takhar <amar@ntp.org>
2012-01-21 15:36:27 -0500

    Add initial code to support running commands on a remote machine.
    Includes A connection pool to re-use connections not in use.

---
+++ slave/buildslave/ssh.py
@@ -0,0 +1,152 @@
+from struct import pack, unpack
+
+from twisted.internet.protocol import ClientCreator
+from twisted.internet import reactor, defer
+
+from twisted.conch.ssh.channel import SSHChannel
+from twisted.conch.ssh.connection import SSHConnection
+from twisted.conch.ssh.transport import SSHClientTransport, SSHTransportBase
+from twisted.conch.ssh.keys import Key
+from twisted.conch.ssh.userauth import SSHUserAuthClient
+defer.setDebugging(True)
+
+
+# BBClientTransport
+class BBClientTransport(SSHClientTransport):
+	def __init__(self, command, argv):
+		self.command = command
+		self.argv = argv
+
+	def verifyHostKey(self, hostKey, fingerprint):
+		# XXX: add hostKey="" to SSHBuildSlave.
+		print "Host fingerprint: %s" % fingerprint
+		return defer.succeed(True)
+
+	def connectionSecure(self):
+		cc = BBSSHConnection(self.command, self.argv)
+		auth = BBAuthClient("admin", cc)
+		self.requestService(auth)
+
+
+# BBAuthClient
+class BBAuthClient(SSHUserAuthClient):
+	def getPublicKey(self):
+		path = "/mnt/devel/vm/bin/key/vm.pub"
+		return Key.fromFile(filename=path).blob()
+
+	def getPrivateKey(self):
+		path = "/mnt/devel/vm/bin/key/vm"
+		return defer.succeed(Key.fromFile(path).keyObject)
+
+
+# BBSSHConnection
+class BBSSHConnection(SSHConnection):
+	def __init__(self, command, argv):
+		SSHConnection.__init__(self)
+		self.command = command
+		self.argv = argv
+
+	def serviceStarted(self):
+		sshpool = self.command.builder.sshpool
+		sshpool.add(self, self.command)
+
+		channel = BBSSHChannel(self.command, self.argv)
+		self.openChannel(channel)
+
+
+# BBSSHChannel
+class BBSSHChannel(SSHChannel):
+	name = 'session'
+
+	def __init__(self, command, argv):
+		SSHChannel.__init__(self)
+		self.command = command
+		self.argv = argv
+
+	def openFailed(self, reason):
+		self.command.deferred.errback(reason)
+
+	def channelOpen(self, ignored):
+		str_cmd = " ".join(self.argv)
+		str_net = pack('!L', len(str_cmd)) + str_cmd
+		self.conn.sendRequest(self, 'exec', str_net)
+
+	def dataReceived(self, data):
+		self.command.addStdout(data)
+
+	def extReceived(self, data):
+		self.command.addStderr(data)
+
+	def closed(self):
+		sig = None
+		rc = 0
+		self.command.finished(sig, rc)
+
+	def request_exit_status(self, data):
+		status = unpack('>L', data)[0]
+		print "BBSSHChannel():request_exit_status(): %s" % status
+#		self.loseConnection()
+		self.command.builder.sshpool.free(self.command)
+
+
+
+class SSHConnectionPool:
+	pool = {}
+	id_count = 0
+
+	def run(self, command, argv):
+		sshslave = command.builder.sshslave
+		name = sshslave["name"]
+
+		for (i, x) in enumerate(self.pool.get(name, [])):
+			if x["id"] is None:
+				id = self.id_get()
+				print "SSHConnectionPool:get(): reusing connection, new id: %s" % id
+				self.pool[name][i]["id"] = id
+				command.builder.sshpool_id = id
+
+				channel = BBSSHChannel(command, argv)
+				return self.pool[name][i]["conn"].openChannel(channel)
+
+		p = ClientCreator(reactor, BBClientTransport, command, argv)
+		d = p.connectTCP(
+			sshslave["host"],
+			sshslave["port"],
+			timeout=sshslave["timeout"],
+			bindAddress=sshslave["bindAddress"])
+
+		# XXX: If sshpool.workdir != workdir insert a deferred to change the directory.
+		return d
+
+
+	def add(self, conn, command):
+		builder = command.builder
+		name = builder.sshslave["name"]
+		builder.sshpool_id = self.id_get()
+
+		new = {
+			"id": builder.sshpool_id,
+			"conn" : conn,
+			"workdir": command.workdir,
+		}
+
+		self.pool.setdefault(name, []).append(new)
+		print "SSHConnectionPool:add(): new connection: %s" % new
+
+
+	def free(self, command):
+		id = command.builder.sshpool_id
+		name = command.builder.sshslave["name"]
+
+		print "SSHConnectionPool:remove(): free connection %s" % id
+
+		for (i, x) in enumerate(self.pool[name]):
+			if x["id"] == id: 
+				self.pool[name][i]["id"] = None
+
+
+	def id_get(self):
+		# locking disaster?
+		self.id_count += 1
+		return self.id_count
+
